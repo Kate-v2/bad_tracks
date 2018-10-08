7f450fae71a04d211490aa0f94d0a822dac47470
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-08 14:16:55 -0600

    rollback the last migration so you can edit that migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008201030) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "songs", force: :cascade do |t|
-    t.string "title"
-    t.integer "length"
-    t.integer "play_count"
-  end
 
 end
