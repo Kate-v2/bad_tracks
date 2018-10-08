d3a2afadbf3c2c9efc6132e423bfbe2d0babf848
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-08 14:07:56 -0600

    add a Song.rb Model file

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+# important that the name is singular!
+# ALL MODELS should have SINGULAR model names
+
+# NOTE Not inheriting from ActiveRecord
+class Song < ApplicationRecord
+  # IF you open this file -- you can make methods that can be passed (bc of inheritance)
+  # that will be passed to ALL of your MODELS
+
+end
