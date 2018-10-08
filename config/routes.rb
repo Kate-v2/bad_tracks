51fcc2fc297f4f9106a8690cf73ba917a4895fde
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-08 14:36:11 -0600

    change method in routes from get to resources which allows any verb combination if you look via rake routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # the method name is index -- from the file songs_controller.rb
-  get '/songs', to: 'songs#index'
+  # get '/songs', to: 'songs#index'
+  resources :songs
 
 end
