6f39ef3dc96e2738349952040bda49e1f24377ae
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-09 10:13:19 -0600

    update spec test with more relationship tests -- we need to setup the join table next

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,8 +4,11 @@ describe Song, type: :model do
 
   describe 'Relationships' do
     it { should belong_to(:artist) }
+    it { should have_many(:playlist_songs) }
+    it { should have_many(:playlists).through(:playlist_songs) }
+
   end
 
-  
+
 
 end
