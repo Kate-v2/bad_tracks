e20bcae684a737dc5b9f72607f05f94ce44d6cff
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-09 10:15:18 -0600

    add a playlist_spec (test) describing relationships between playlist & playlist_songs & songs

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -6,7 +6,6 @@ describe Song, type: :model do
     it { should belong_to(:artist) }
     it { should have_many(:playlist_songs) }
     it { should have_many(:playlists).through(:playlist_songs) }
-
   end
 
 
