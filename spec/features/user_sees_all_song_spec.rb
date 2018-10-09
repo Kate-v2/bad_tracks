2f6b14f4bc9ce60c3ff58895d1e897cb1ff298e0
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-09 10:00:40 -0600

    remove the bang - we need to add an artist to our test

--- spec/features/user_sees_all_song_spec.rb
+++ spec/features/user_sees_all_song_spec.rb
@@ -7,8 +7,8 @@ require 'rails_helper'
 describe 'user index' do
 
   it 'user can see all songs' do
-    song_1 = Song.create!(title: "Title 1", length: 100, play_count: 10)
-    song_2 = Song.create!(title: "Title 2", length: 200, play_count: 20)
+    song_1 = Song.create(title: "Title 1", length: 100, play_count: 10)
+    song_2 = Song.create(title: "Title 2", length: 200, play_count: 20)
 
     visit '/songs'
 
