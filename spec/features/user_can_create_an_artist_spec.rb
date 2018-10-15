f15d301d8603360afab3065f50d814a4648775f4
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-15 09:18:30 -0600

    write test for 'artists/new' -- but no artist model exists yet

--- spec/features/user_can_create_an_artist_spec.rb
+++ spec/features/user_can_create_an_artist_spec.rb
@@ -4,6 +4,16 @@ require "rails_helper"
 describe 'User can create an artist' do
 
   it 'when visiting /artists/new' do
+    visit '/artists/new'
+    artist_name = "Nickleback"
+
+    fill_in('artist[name]', with: artist_name)
+
+    # click_link & click_button are more specific, but click_on will work for either
+    click_on('Create Artist')
+
+    expect(current_path).to eq("artists/#{Artist.last.id}")
+    expect(page).to have_content(artist_name)
 
   end
 
