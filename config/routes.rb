9c9e01d0e939e262e0078127d06c8be405cb9e41
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-15 13:55:04 -0600

    nest a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
-Rails.application.routes.draw do
+
+# Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # the method name is index -- from the file songs_controller.rb
   # get '/songs', to: 'songs#index'
+  # resources :songs, only: [:index]
+  #
+  # resources :artists, only: [:new, :create, :show]
+  #
+  # resources :playlist, only: [:name, :create, :show]
+
+# end
+
+
+Rails.application.routes.draw do
+
+  resources :artists, only: [:new, :create, :show] do
+    # Nested Resource
+    resources :songs, only: [:new]
+  end
+
   resources :songs, only: [:index]
-
-  resources :artists, only: [:new, :create, :show]
-
-  resources :playlist, only: [:name, :create, :show]
 
 end
