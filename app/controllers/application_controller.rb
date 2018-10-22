10c1dcaa8a743d1aef88be56487c1f1e57862ba8
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-22 11:27:24 -0600

    helper method for current user in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,19 @@ class ApplicationController < ActionController::Base
 
   before_action :set_breadcrumb
 
-
-
-
-
   def set_breadcrumb
     # what it current or new if it doesn't exist yet
     @breadcrumbs ||= session[:breadcrumbs]
     @breadcrumbs.push(request.path_info)
   end
+
+
+  # this lets the view access the helper method
+  helper_method :current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 
 
 end
