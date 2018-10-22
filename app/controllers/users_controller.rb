11c5568f89c195bb74b295ff5e525b9500dc4711
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-22 09:38:42 -0600

    new user form with password field type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+  end
+
+  def create
+
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,6 @@ Rails.application.routes.draw do
 
   resources :songs, only: [:index]
 
-  resources :users, only:[:new]
+  resources :users, only:[:new, :create]
 
 end
