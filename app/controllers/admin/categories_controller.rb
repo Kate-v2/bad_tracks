9ee024a9e0ba9f5032b5ad98bc72b4bd94698d91
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-22 15:23:58 -0600

    admin categories controller require_admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,8 +3,21 @@
 class Admin::CategoriesController < ApplicationController
   # must include namespaced name (admin)
 
+  # Before any other action method gets called, go do this other work
+  # this is a filter
+  before_action :require_admin
+
   def index
-    
+
+  end
+
+
+  private
+
+  def require_admin
+    # are you logged in, if you're logged in are you an admin, do you have permission to see this
+    render file: "/public/404" unless current_admin?
+    end
   end
 
 
