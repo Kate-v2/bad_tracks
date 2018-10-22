9f5aae461f7a0994bbd46d639367217767558f41
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-22 11:18:58 -0600

    create method guts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,10 @@ class sessionsController < ApplicationController
   end
 
   def create
+    # user = User.find_by(username: params[:username])
+    # Active Record gives us:
+    user = User.find_by_username(params[:username])
   end
+
 
 end
