7396c7742427bbbba933ef6f86c557b805bb99e2
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-22 10:32:45 -0600

    sad path is tested, we can now add it if we want

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,7 @@ class UsersController < ApplicationController
   def create
     # @user = User.create(user_params)
     @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      # "Sad Path" -- we shouldn't have this because we don't test it
-      render :new
-    end
+    redirect_to user_path(@user) if @user.save
   end
 
   def show
--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -7,18 +7,22 @@ describe 'user registration' do
     # Important to test: -anonymous user, -registered user, -admin user, etc..
     visit '/'
     # visit root_path
-
     click_on 'Sign up to be a new user'
-
     username = 'Fred'
-
     expect(current_path).to eq(new_user_path)
     # :user_username => "user[:username]"
     fill_in :user_username, with: username
     fill_in :user_password, with: "password1"
     click_on 'Create User'
     expect(page).to have_content("Welcome #{username}")
-
   end
+
+  it 'anonymous visitor fails registration' do
+    visit new_user_path
+    click_on 'Create User'
+    expect(current_path).to eq(users_path)
+    expect(page).to have_button("Create User")
+  end
+
 
 end
