f601652c93fb591f2ce2dce5d36a9a932e4248df
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-22 10:47:26 -0600

    typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 
 class User < ApplicationRecord
 
-  validate_presence_of :password, require: true
+  validates_presence_of :password, require: true
   # REQUIRE adds the visual to the form that says it can't be submitted without an entry into that field
   # Modern browsers take this require attribute and add the visual without needing to address how that appears in our html/css
   # Also -- it prevents the form from being submitted (and quitely failing to create) without the required field
-  validate :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 
 
   # via bcrypt gem -- method mixin
