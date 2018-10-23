24074cd1f9376d9a1b3fc6b797326d553ac3c51c
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-23 09:22:43 -0600

    Sequence Dynamic Values

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -2,9 +2,12 @@
 FactoryBot.define do
 
   factory :song do
-    title {"Don't Stop Believin"}
-    length { 50 }
-    play_count { 100 }
+    # title {"Don't Stop Believin"}
+    sequence(:title) { |n| "Song #{n}"}
+    # length { 50 }
+    sequence(:lenght) { |n| 10*n }
+    # play_count { 100 }
+    sequence(:play_count) { |n| 100*n }
     artist   # this creates a reference
   end
 
