de4da869afa1241e35f594a42eeeec5a37d9b5f0
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-25 11:29:07 -0600

    flash actually goes into another file

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,16 @@
 class CartsController < ApplicationController
 
   def create
+
+    binding.pry
+
+
+    song = Song.find(params[:song_id])
+    # song = Song.find(params[:id])
+    # song = Song.find(params[:song][:id])
+
+    flash[:notice] = "You now have 1 copy of #{song.title} in your cart."
+    redirect_to songs_path
   end
 
 end
