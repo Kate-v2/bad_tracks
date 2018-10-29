bd7afaccd20ed72c24f2f08e001a579ccf1e65e8
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-29 10:50:08 -0600

    nav is static and we're about to blow up everything by making dynamic

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,16 @@ class CartsController < ApplicationController
 
   def create
     song = Song.find(params[:song_id])
-    flash[:notice] = "You have one copy of #{song.title}"
+
+    song_id_str = song.id.to_s
+
+    session[:cart] ||= Hash.new(0)       # zero is there but upon using, it's a STRING / this is for readability
+    session[:cart][song_id_str] ||= 0   # so because it's a string 0, we just reset it to 0 as a int for working with below
+    # session[:cart][song_id_str].to_i   <--- can I do this?
+    session[:cart][song_id_str] = session[:cart][song_id_str] + 1
+
+    quantity = session[:cart][song_id_str]
+    flash[:notice] = "You have #{pluralize(quantity, "copy")} copy of #{song.title}"
     redirect_to songs_path
   end
 
--- spec/features/user_can_add_songs_to_cart_spec.rb
+++ spec/features/user_can_add_songs_to_cart_spec.rb
@@ -5,19 +5,28 @@ RSpec.describe 'User can add songs to cart' do
 
   it 'shows a flash message when a user adds a song' do
     artist = Artist.create(name: 'Name 1')
-    song   = artist.songs.create( tite: 'Title 1', length: 100, play_count: 1000)
+    song   = artist.songs.create( title: 'Title 1', length: 100, play_count: 1000)
     visit song_path; click_button 'Add Song'
     expect(page).to gave_content('You now have ')
   end
 
   it 'shows a flash message when a user adds a song again' do
     artist = Artist.create(name: 'Name 1')
-    song   = artist.songs.create( tite: 'Title 1', length: 100, play_count: 1000)
+    song   = artist.songs.create( title: 'Title 1', length: 100, play_count: 1000)
     visit song_path; click_button 'Add Song'
     visit song_path; click_button 'Add Song'
     expect(page).to gave_content('You now have ')
   end
 
+  it 'shows our cart quantity in the nav area' do
+    artist = Artist.create(name: 'Name 1')
+    song   = artist.songs.create( title: 'Title 1', length: 100, play_count: 1000)
+    expect(page).to have_content("Cart: 0")
+    visit song_path; click_button 'Add Song'
+    expect(page).to have_content("Cart: 1")
+    visit song_path; click_button 'Add Song'
+    expect(page).to have_content("Cart: 2")
+  end
 
 
 end
