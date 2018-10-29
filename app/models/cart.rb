fd319ed4b2e68d0848c3b69fff092ce3a2f67a8a
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-29 11:24:01 -0600

    methods in cart PORO

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 
 # PORO - Plain Old Ruby Object
 
-class Cart
+class Cart  
 
   attr_reader :contents
 
@@ -11,6 +11,14 @@ class Cart
 
   def total_count
     @content.values.sum
+  end
+
+  def add_song(song_id)
+    @contents[song_id.to_s] += 1
+  end
+
+  def count_of(song_id)
+    @content[songs_id.to_s].to_i
   end
 
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -9,5 +9,19 @@ RSpec.describe Cart do
   end
 
 
+  it 'add song' do
+    cart = Cart.new( {"1" => 2, "2" => 3} )
+    cart.add_song(1) #  <-- song ids
+    cart.add_song(2)
+    expect(cart.contents).to eq( {"1" => 3, "2" => 4} )
+  end
+
+  it 'count of' do
+    cart = Cart.new({})
+    # count_of(song_id)
+    expect(cart.count_of(5)).to eq(0)
+
+  end
+
 
 end
